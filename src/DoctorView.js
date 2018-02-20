ca2974ed279dfee9f146f39431d3d75efbbccc9a
xofmai <35841552+xofmai@users.noreply.github.com>
2018-02-20 10:00:35 -0600

    doctor card skeleton

--- src/DoctorView.js
+++ src/DoctorView.js
@@ -16,6 +16,41 @@ class DoctorView extends Component {
         <h1 className="pageHeader">
           DoctorView
         </h1>
+        <div className="card" style={{ width: '900px', display: 'inline-block', float: 'center' }}>
+        <div>
+          <div className="row">
+            <div className="small-6 medium-6 large-6 xlarge-6 columns">
+              <img src="https://www.permanente.net/pmdb/photosync/2104581_photoweb.jpg" />
+              <h2 style={{ topPadding: '20px' }}>Languages Spoken</h2>
+              <p>English</p>
+              <h2>Publications</h2>
+              <p className="docBio">Science Journal Weekly | Medical Roundup | DocAdvice 2017 | You Ate What? </p>
+              <h2>Insurance Compatibility</h2>
+              <p className="docBio">Humana | BlueCross BlueShield | OuchMyFoot | ThatsGoingToNeedASecondOpinion</p>
+              <h2>Find Me</h2>
+              <p className="docBio">Address: 3600 Broadway
+                 Oakland, CA 94611</p>
+              <p className="docBio">Phone: 510-752-5438</p>
+            </div>
+            <div className="small-6 medium-6 large-6 xlarge-6 columns">
+              <h1>Meet Doctor Gary</h1>
+              <h2>Biography</h2>
+              <p className="docBio scrollBox">Rodney Erickson is a content marketing professional at HubSpot, an inbound marketing and sales platform that helps companies attract visitors, convert leads, and close customers. Previously, Rodney worked as a marketing manager for a tech software startup. He graduated with honors from Columbia University with a dual degree in Business Administration and Creative Writing. Rodney Erickson is a content marketing professional at HubSpot, an inbound marketing and sales platform that helps companies attract visitors, convert leads, and close customers. Previously, Rodney worked as a marketing manager for a tech software startup. He graduated with honors from Columbia University with a dual degree in Business Administration and Creative Writing. Rodney Erickson is a content marketing professional at HubSpot, an inbound marketing and sales platform that helps companies attract visitors, convert leads, and close customers. Previously, Rodney worked as a marketing manager for a tech software startup. He graduated with honors from Columbia University with a dual degree in Business Administration and Creative Writing."</p>
+              <h2>Education</h2>
+              <p className="docBio">University of Cincinnati College of Medicine |
+                San Joaquin General Hospital |
+                Kaiser Permanente Medical Center Oakland CA |
+                San Joaquin General Hospital |
+                Cleveland Clinic Foundation </p>
+              <h2>Specialties</h2>
+              <div className="docBio"></div>
+              <div className="scrollBox">Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles
+                Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, AnklesAnkles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     );
   }

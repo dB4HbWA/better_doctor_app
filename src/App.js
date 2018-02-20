d2007084c2aeb583479d18c2861c3b5b02f378bf
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-20 13:11:10 -0600

    ready for firebase call

--- src/App.js
+++ src/App.js
@@ -49,12 +49,23 @@ const mapStateToProps = state => {
   };
 };
 
+const SignInInput = props => {
+  return (
+    <div style={{display: 'inline-block', width:'50%'}}>
+      <input onChange={props.updateUserName} style={{display: 'inline-block', width:'40%', marginRight: '2%'}} type='text' value={props.signInUserName} placeholder='Username' />
+      <input onChange={props.updatePassword} style={{display: 'inline-block', width:'40%', marginRight: '2%'}} type='text' value={props.signInPassword} placeholder='Password' />
+      <button onClick={props.handleSignOnClick} style={{display: 'inline-block', marginRight: '2%'}}>Go</button>
+    </div>
+  )
+}
+
 const SignInBar = props => {
   return (
-    <div style={{textAlign: 'right'}}>
-      <div style={{display: 'inline-block'}}>Sign In</div>
-      <div style={{display: 'inline-block'}}>/</div>
-      <Link style={{display: 'inline-block'}}to={'/newProfile'} >Create Profile</Link>
+    <div style={{ textAlign: 'right' }}>
+      {props.enteringSignInInfo && <SignInInput handleSignOnClick={props.handleSignOnClick} updatePassword={props.updatePassword} updateUserName={props.updateUserName} signInUserName={props.signInUserName} signInPassword={props.signInPassword} />}
+      <div onClick={props.handleSignInStatusChange} className={'inline clickableSignon'}>{!props.enteringSignInInfo && 'Sign In'}</div>
+      <div style={{ display: 'inline-block' }}>/</div>
+      <Link style={{ display: 'inline-block' }} to={'/newProfile'} >Create Profile</Link>
     </div>
   )
 }
@@ -62,12 +73,44 @@ const SignInBar = props => {
 const NavBarWrapped = connect(mapStateToProps)(NavBar)
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      enteringSignInInfo: false,
+      signInUserName: "",
+      signInPassword: ""
+    }
+    this.handleSignInStatusChange = this.handleSignInStatusChange.bind(this)
+    this.updateUserName = this.updateUserName.bind(this)
+    this.updatePassword = this.updatePassword.bind(this)
+    this.handleSignOnClick = this.handleSignOnClick.bind(this)
+  }
+
+  handleSignInStatusChange() {
+    if (this.state.enteringSignInInfo)
+      this.setState({ enteringSignInInfo: false })
+    else
+      this.setState({ enteringSignInInfo: true })
+  }
+
+  updateUserName(event) {
+    this.setState({ signInUserName: event.target.value })
+  }
+
+  updatePassword(event) {
+    this.setState({ signInPassword: event.target.value })
+  }
+
+  handleSignOnClick() {
+    
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
           <div className="appHeader">
-            <SignInBar />
+            <SignInBar signInUserName={this.state.signInUserName} signInPassword={this.state.signInPassword} handleSignOnClick={this.handleSignOnClick} updatePassword={this.updatePassword} updateUserName={this.updateUserName} handleSignInStatusChange={this.handleSignInStatusChange} enteringSignInInfo={this.state.enteringSignInInfo} />
             <NavBarWrapped />
           </div>
           <Switch>
@@ -75,7 +118,7 @@ class App extends Component {
             <Route exact path="/mydoctors" component={FavoritesView} />
             <Route exact path="/profile" component={ProfileView} />
             <Route exact path="/newProfile" component={NewProfileView} />
-            <Route exact path="/doctor/:uid" render={({match}) => <DoctorView match={match} />} />
+            <Route exact path="/doctor/:uid" render={({ match }) => <DoctorView match={match} />} />
           </Switch>
         </div>
       </Router>

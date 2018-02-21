033473011f6bc4d6ad5ffde6a3c406456cc8da90
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-21 08:59:35 -0600

    still good

--- src/state/actions.js
+++ src/state/actions.js
@@ -25,22 +25,6 @@ export function loadDocData(filter) {
 }
 
 export function loadFavorites(uid) {
-  //   return (dispatch, getState, api) => {
-
-  //   const location = filter.location.lat + ',' + filter.location.lng
-  //   const miles = filter.miles
-
-  //   const promise = axios.get('https://api.betterdoctor.com/2016-03-01/doctors?location=' + location + ',' + miles + '&sort=distance-asc&skip=0&limit=50&user_key=1beb2ecd945d9c2a3079c77dc33129ce');
-
-  //   promise.then(({ data: docData }) => {
-  //     console.log(docData)
-  //     dispatch({ type: LOAD_DOC_DATA, payload: docData.data })
-  //   }, () => { })
-  //   promise.catch((data) => {
-  //     console.log(data)
-  //   }
-  // )
-  // }
   return (dispatch, getState, api) => {
     var database = firebase.database();
     var ref = firebase.database().ref();

cd216ba4f73699d2608f80fd53aff746773f5f36
xofmai <35841552+xofmai@users.noreply.github.com>
2018-02-21 00:31:40 -0600

    doctor view updated

--- src/DoctorView.js
+++ src/DoctorView.js
@@ -13,17 +13,23 @@ class DoctorView extends Component {
   constructor(props) {
     super(props)
     doctorViewed: undefined
+    this.state ={
+      doctorDetail: undefined
+    }
   }
 
   componentDidMount() {
     console.log('https://api.betterdoctor.com/2016-03-01/doctors/' + this.props.match.params.uid + '?user_key=1beb2ecd945d9c2a3079c77dc33129ce')
     const promise = axios.get('https://api.betterdoctor.com/2016-03-01/doctors/' + this.props.match.params.uid + '?user_key=1beb2ecd945d9c2a3079c77dc33129ce');
     promise.then(({ data: docData }) => {
-      console.log(docData)
+      console.log("componentDidMount")
+      console.log(docData.data)
+      this.setState({doctorDetail: docData.data});
+
     }, () => { })
     promise.catch((data) => {
       console.log(data)
-    }
+    } 
   )
   }
 
@@ -31,43 +37,43 @@ class DoctorView extends Component {
     return (
       <div>
         <h1 className="pageHeader">
-          DoctorView
+          Doctor Details
         </h1>
+        {this.state.doctorDetail !== undefined &&
         <div className="card" style={{ width: '900px', display: 'inline-block', float: 'center' }}>
           <div>
-            <div className="row">
+            <div>
               <div className="small-6 medium-6 large-6 xlarge-6 columns">
-                <img src="https://www.permanente.net/pmdb/photosync/2104581_photoweb.jpg" />
-                <h2 style={{ topPadding: '20px' }}>Languages Spoken</h2>
-                <p>English</p>
-                <h2>Publications</h2>
+                <img src={this.state.doctorDetail.profile.image_url} style={{ padding: '30px' }} />
+                <h2>Languages Spoken</h2>
+                <p>{this.state.doctorDetail.profile.languages.map((language) => language.name).join(', ')}</p>
+                <h2></h2>
                 <p className="docBio">Science Journal Weekly | Medical Roundup | DocAdvice 2017 | You Ate What? </p>
                 <h2>Insurance Compatibility</h2>
-                <p className="docBio">Humana | BlueCross BlueShield | OuchMyFoot | ThatsGoingToNeedASecondOpinion</p>
-                <h2>Find Me</h2>
+                <p className="docBio scrollBox">{this.state.doctorDetail.insurances.map((insurance) => insurance.insurance_provider.name).join(', ')}</p>
+                <h2>Find Me - update this</h2>
                 <p className="docBio">Address: 3600 Broadway
                  Oakland, CA 94611</p>
                 <p className="docBio">Phone: 510-752-5438</p>
               </div>
               <div className="small-6 medium-6 large-6 xlarge-6 columns">
-                <h1>Meet Doctor Gary</h1>
+                <h2>Meet Doctor {this.state.doctorDetail.profile.first_name + ' ' + 
+                  this.state.doctorDetail.profile.last_name  + ' ' + this.state.doctorDetail.profile.title}
+                </h2>
+                <div></div>
                 <h2>Biography</h2>
-                <p className="docBio scrollBox">Rodney Erickson is a content marketing professional at HubSpot, an inbound marketing and sales platform that helps companies attract visitors, convert leads, and close customers. Previously, Rodney worked as a marketing manager for a tech software startup. He graduated with honors from Columbia University with a dual degree in Business Administration and Creative Writing. Rodney Erickson is a content marketing professional at HubSpot, an inbound marketing and sales platform that helps companies attract visitors, convert leads, and close customers. Previously, Rodney worked as a marketing manager for a tech software startup. He graduated with honors from Columbia University with a dual degree in Business Administration and Creative Writing. Rodney Erickson is a content marketing professional at HubSpot, an inbound marketing and sales platform that helps companies attract visitors, convert leads, and close customers. Previously, Rodney worked as a marketing manager for a tech software startup. He graduated with honors from Columbia University with a dual degree in Business Administration and Creative Writing."</p>
+                <p className="docBio scrollBox">
+                  {this.state.doctorDetail.profile.bio}</p>
                 <h2>Education</h2>
-                <p className="docBio">University of Cincinnati College of Medicine |
-                  San Joaquin General Hospital |
-                  Kaiser Permanente Medical Center Oakland CA |
-                  San Joaquin General Hospital |
-                Cleveland Clinic Foundation </p>
+                <p className="docBio">{this.state.doctorDetail.educations.map((education) => education.school).join(', ')}ed</p>
                 <h2>Specialties</h2>
                 <div className="docBio"></div>
-                <div className="scrollBox">Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles
-                  Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, AnklesAnkles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles, Hips, Knees, Ankles
-                </div>
+                <div className="scrollBox">{this.state.doctorDetail.specialties.map((specialty) => specialty.name).join(', ')} </div>
               </div>
             </div>
           </div>
         </div>
+        }
       </div>
     );
   }
